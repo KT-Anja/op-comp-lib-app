6baf30b4cbb902b3f2d1c42974a568d08f82b658
Anja-Janina Stiefermann <anja.stiefermann@kernpunkt.de>
2022-05-13 12:57:32 +0200

    fix typescript errors

--- src/pages/progress.tsx
+++ src/pages/progress.tsx
@@ -6,9 +6,9 @@ const Progress = () => {
 					className="progress-bar progress-bar-striped bg-primary"
 					role="progressbar"
 					style={{ width: '25%' }}
-					aria-valuenow="25"
-					aria-valuemin="0"
-					aria-valuemax="100"
+					aria-valuenow={25}
+					aria-valuemin={0}
+					aria-valuemax={100}
 				></div>
 			</div>
 			<div className="progress mb-3">
@@ -16,9 +16,9 @@ const Progress = () => {
 					className="progress-bar progress-bar-striped bg-primary"
 					role="progressbar"
 					style={{ width: '50%' }}
-					aria-valuenow="50"
-					aria-valuemin="0"
-					aria-valuemax="100"
+					aria-valuenow={50}
+					aria-valuemin={0}
+					aria-valuemax={100}
 				></div>
 			</div>
 			<div className="progress mb-3">
@@ -26,9 +26,9 @@ const Progress = () => {
 					className="progress-bar progress-bar-striped bg-primary"
 					role="progressbar"
 					style={{ width: '75%' }}
-					aria-valuenow="75"
-					aria-valuemin="0"
-					aria-valuemax="100"
+					aria-valuenow={75}
+					aria-valuemin={0}
+					aria-valuemax={100}
 				></div>
 			</div>
 			<div className="progress mb-3">
@@ -36,9 +36,9 @@ const Progress = () => {
 					className="progress-bar progress-bar-striped bg-primary"
 					role="progressbar"
 					style={{ width: '100%' }}
-					aria-valuenow="100"
-					aria-valuemin="0"
-					aria-valuemax="100"
+					aria-valuenow={100}
+					aria-valuemin={0}
+					aria-valuemax={100}
 				></div>
 			</div>
 		</>
