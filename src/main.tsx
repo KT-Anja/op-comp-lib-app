d957c9923ade32810b923bbcbe0ad669560aa871
Anja-Janina Stiefermann <anja.stiefermann@kernpunkt.de>
2022-05-13 14:50:51 +0200

    fix types/react version problems

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-
-import Start from './preview/start'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Start from "./pages/start";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Router>
-			<Start />
-		</Router>
-	</React.StrictMode>,
-	document.getElementById('root')
-)
+  <React.StrictMode>
+    <Router>
+      <Start />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
